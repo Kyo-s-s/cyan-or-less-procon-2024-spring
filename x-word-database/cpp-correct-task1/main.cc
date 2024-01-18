0e87d302c82e9660d36ed8c9721b8fa7d26e842c
Horike Haruki <igagrid407@gmail.com>
2024-01-19 02:23:14 +0900

    [Fix]

--- x-word-database/cpp-correct-task1/main.cc
+++ x-word-database/cpp-correct-task1/main.cc
@@ -10,15 +10,33 @@ int main() {
     assert(X <= 8);
 
     ll ans = 0;
-    vector<string> cyanstrs;
+    vector<string> cyanstrs = {"cyan"};
+
     for (char a = 'a'; a <= 'z'; a++) {
         string x = string(1, a);
+
+        cyanstrs.push_back(x + "cyan");
+        cyanstrs.push_back("cyan" + x);
+
         for (char b = 'a'; b <= 'z'; b++) {
             string y = string(1, b);
+
+            cyanstrs.push_back(x + y + "cyan");
+            cyanstrs.push_back(x + "cyan" + y);
+            cyanstrs.push_back("cyan" + x + y);
+
+
             for (char c = 'a'; c <= 'z'; c++) {
                 string z = string(1, c);
+
+                cyanstrs.push_back(x + y + z + "cyan");
+                cyanstrs.push_back(x + y + "cyan" + z);
+                cyanstrs.push_back(x + "cyan" + y + z);
+                cyanstrs.push_back("cyan" + x + y + z);
+
                 for (char d = 'a'; d <= 'z'; d++) {
                     string w = string(1, d);
+
                     cyanstrs.push_back(x + y + z + w + "cyan");
                     cyanstrs.push_back(x + y + z + "cyan" + w);
                     cyanstrs.push_back(x + y + "cyan" + z + w);

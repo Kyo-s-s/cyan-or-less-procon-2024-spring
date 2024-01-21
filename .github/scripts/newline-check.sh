f090773b96b5eb06019a378db144d5e06564597a
Horike Haruki <igagrid407@gmail.com>
2024-01-21 16:41:45 +0900

    [Fix]

--- .github/scripts/newline-check.sh
+++ .github/scripts/newline-check.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-res=true
-find . -type f | while read -r file; do
+fall=false
+
+while IFS= read -r -d '' file; do
     if [[ $(tail -c 1 "$file" | wc -l) != $'1' ]]; then
         echo "'$file' does not end with a newline."
-        res=false
+        fall=true
     fi
-    
-    if [[ res == false ]]; then
-        exit 1
-    fi
-done
+done < <(find . -type f -print0)
+
+if $fall; then
+    exit 1
+fi

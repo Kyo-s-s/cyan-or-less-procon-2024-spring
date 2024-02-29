6868063209cf9e461b903fb227b333b1f7449d45
Kyo_s_s <igagrid407@gmail.com>
2024-02-29 19:49:24 +0900

    [Fix] solution

--- subset-mex/py-correct-task2/main.py
+++ subset-mex/py-correct-task2/main.py
@@ -6,21 +6,27 @@ N = int(input())
 A = list(map(int, input().split()))
 
 
-cnt = [0 for _ in range(1010)]
+cnt = [0 for _ in range(1002)]
 for a in A:
     cnt[a] += 1
 
 ans = 0
-for mex in range(1005):
+for mex in range(1002):
     c = 1
     for i in range(mex):
         c *= pow(2, cnt[i], mod) - 1
-        c %= mod
-    for i in range(mex + 1, 1010):
-        c *= pow(2, cnt[i], mod)
+        if c >= mod:
+            c %= mod
+    cs = 0
+    for i in range(mex + 1, 1002):
+        cs += cnt[i]
+    
+    c *= pow(2, cs, mod)
+    if c >= mod:
         c %= mod
     
     ans += mex * c
-    ans %= mod
+    if ans >= mod:
+        ans %= mod
 
 print(ans)

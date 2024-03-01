7440c59e4e7656c2bb6168bd1db0798bfcdfdebc
yasunori <0528.taiki.sax@gmail.com>
2024-03-01 18:50:24 +0900

    fix main.cc

--- long-chess-board/cpp-correct/main.cc
+++ long-chess-board/cpp-correct/main.cc
@@ -26,15 +26,16 @@ vector<pair<int,int>> dir_b = {{1, 1}, {2, 2}, {-1, 1}, {-2, 2}, {-1, -1}, {-2, 
 int64_t f_dijkstra(int H, int W, int r_k, int c_k, int r_b, int c_b){
     //これを使うのは (H==3 && dy < 5) のとき
     //そのため2つのコマは3*4の長方形に収まっている
-    //盤が3*4以上のときナイトは全てのマスに移動可能なので、ans <= 11 or ans == INF
-    //いずれかのコマから見て横方向の距離が22より大きいマスは集合場所になりえない
-
+    //盤が3*4のときナイトは任意のマスから任意のマスへ5回以下で移動できる
+    //ceil(abs(c_k-y)/2) + ceil(abs(c_b-y)/2) > 5 となるマス(x, y)は答えにならない
+    //よって左右の余白は4マスあれば十分
+    
     int L = 0;
-    chmax(L, c_k - 22);
-    chmax(L, c_b - 22);
+    chmax(L, c_k - 4);
+    chmax(L, c_b - 4);
     int R = W;
-    chmin(R, c_k + 23);
-    chmin(R, c_b + 23);
+    chmin(R, c_k + 5);
+    chmin(R, c_b + 5);
 
     vector<vector<int64_t>> D_k(H, vector<int64_t>(R-L, INF));
     vector<vector<int64_t>> D_b(H, vector<int64_t>(R-L, INF));
@@ -105,7 +106,7 @@ int64_t f(int H, int W, int r_k, int c_k, int r_b, int c_b){
         return ans;
     }
     //dy>=5のとき1つの例外を除いてans=ceil(dy/2) (ビショップの横方向の移動距離が3以下で実現できる)
-    int dx = abs(r_b-r_k);
+    //int dx = abs(r_b-r_k);
     int dy = abs(c_b-c_k);
     if(dy < 5) return f_dijkstra(H, W, r_k, c_k, r_b, c_b);
     if(r_b == 1 && ((r_k == 1 && dy % 4 == 2) || (r_k != 1 && dy % 4 == 0))) return (dy+1)/2 + 1;

b6eae77e9e0353f9022d6f704f079c4d221cefdb
Nichi10p <88943573+Nichi10p@users.noreply.github.com>
2024-03-02 17:13:19 +0900

    validator に抽象化を適用

--- deque-inversion/tests/validator.py
+++ deque-inversion/tests/validator.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 from collections import deque
+from writertools import *
 
 def main():
   N = int(input())
   A = deque(map(int, input().split()))
-  assert 2 <= N <= 10**5
-  assert all(-10**5 <= a <= 10**5 for a in A)
+  assert Constraints.N_MIN <= N <= Constraints.N_MAX
+  assert all(Constraints.X_MIN <= a <= Constraints.X_MAX for a in A)
   Q = int(input())
-  assert 1 <= Q <= 10**5
+  assert Constraints.Q_MIN <= Q <= Constraints.Q_MAX
   for _ in range(Q):
     T, X = input().split()
     X = int(X)
-    assert T in ['push_front', 'push_back', 'pop_front', 'pop_back']
-    assert -10**5 <= X <= 10**5
-    if 'pop' in T:
+    assert T in Constraints.T_LIST
+    assert Constraints.X_MIN <= X <= Constraints.X_MAX
+    if type_is_pop(T):
       assert X == 0
-    if T == 'push_front':
+    if type_is_push(T) and type_is_front(T):
       A.appendleft(X)
-    if T == 'push_back':
+    if type_is_push(T) and type_is_back(T):
       A.append(X)
-    if T == 'pop_front':
+    if type_is_pop(T) and type_is_front(T):
       A.popleft()
-    if T == 'pop_back':
+    if type_is_pop(T) and type_is_back(T):
       A.pop()
     assert len(A) >= 2
 

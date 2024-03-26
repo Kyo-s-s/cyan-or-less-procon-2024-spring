f2106dbd3cad1688707f777ee682b29f820ae685
ack61 <85378035+ack61@users.noreply.github.com>
2024-03-26 22:42:34 +0900

    validatorを追加
    現時点では形式的なチェックしかできない。

---
+++ colorful-balls/tests/validator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+def main():
+    line = re.match(r'^(\d+)$', sys.stdin.readline())
+    assert line, 'Does not match with regexp'
+    Q = int(line.groups()[0])
+    assert 1 <= Q <= 200000, 'T out of range: %d' % Q
+
+    for _ in range(Q):
+        readline = sys.stdin.readline()
+        line = re.match(r'^(1 \d+ \d+|2 \d+|3 \d+)$', readline)
+        assert line, 'Does not match with regexp'
+        query = list(map(int, readline.split()))
+
+        t = query[0]
+        assert 1 <= t <= 3, 't out of range: %d' % t
+        x = query[1]
+        if t == 1:
+            c = query[2]
+            assert 1 <= c <= 1000000000, 'x out of range: %d' % c
+            assert 1 <= x <= 1000000000, 'x out of range: %d' % x
+        elif t == 2:
+            assert 1 <= x <= 1000000000000000000, 'x out of range: %d' % x
+        else:
+            assert 1 <= x <= 1000000000000000000, 'x out of range: %d' % x
+
+    assert sys.stdin.read() == '', 'Extra lines'
+
+
+if __name__ == '__main__':
+    main()

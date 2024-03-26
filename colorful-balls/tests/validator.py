a02c0d718b30510a3bd37d63dd86bd51fc835002
ack61 <85378035+ack61@users.noreply.github.com>
2024-03-26 22:47:53 +0900

    制約を満たさない入力をきちんと弾くようにした

--- colorful-balls/tests/validator.py
+++ colorful-balls/tests/validator.py
@@ -9,6 +9,8 @@ def main():
     Q = int(line.groups()[0])
     assert 1 <= Q <= 200000, 'T out of range: %d' % Q
 
+    ballsNum = 0
+
     for _ in range(Q):
         readline = sys.stdin.readline()
         line = re.match(r'^(1 \d+ \d+|2 \d+|3 \d+)$', readline)
@@ -20,12 +22,14 @@ def main():
         x = query[1]
         if t == 1:
             c = query[2]
-            assert 1 <= c <= 1000000000, 'x out of range: %d' % c
+            assert 1 <= c <= 1000000000, 'c out of range: %d' % c
             assert 1 <= x <= 1000000000, 'x out of range: %d' % x
+            ballsNum += x
         elif t == 2:
-            assert 1 <= x <= 1000000000000000000, 'x out of range: %d' % x
+            assert 1 <= x <= ballsNum, 'x out of range: %d' % x
+            ballsNum -= x
         else:
-            assert 1 <= x <= 1000000000000000000, 'x out of range: %d' % x
+            assert 1 <= x <= ballsNum, 'x out of range: %d' % x
 
     assert sys.stdin.read() == '', 'Extra lines'
 
